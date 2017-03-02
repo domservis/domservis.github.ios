7891b0abc07456c9696c2df31cc38b663b9a4a62
domservis <Mr-redi@mail.ru>
2017-03-02 06:02:31 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -57,23 +57,4 @@ doc.on('ready', init);
 
 
 
-window.ChatraSetup = {
-    mode: 'frame',
-    injectTo: 'chatra-wrapper' 
-};
-    ChatraID = 'feimekTqYwDJ6bPhx';
-    (function(d, w, c) {
-        var n = d.getElementsByTagName('script')[0],
-            s = d.createElement('script');
-        w[c] = w[c] || function() {
-            (w[c].q = w[c].q || []).push(arguments);
-        };
-        s.async = true;
-        s.src = (d.location.protocol === 'https:' ? 'https:': 'http:')
-            + '//call.chatra.io/chatra.js';
-        n.parentNode.insertBefore(s, n);
-    })(document, window, 'Chatra');
-	
-	   Chatra('setColors', {
-    chatBg: 'transparent' // фон
-});
+
